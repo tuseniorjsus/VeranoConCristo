5cec25bc21b27511744b618f65d17287f95fadb0
louieliet <louieliet2003@gmail.com>
2022-05-26 13:57:41 -0500

    Added shoot and following camera mechanics
    Agregué código y archivos para
    tratar las mecánicas de los disparos
    y que la cámara siga al jugador

---
+++ RandomStuff/BulletScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{   
+
+    public float Speed;
+    private Rigidbody2D Rigidbody2D;
+    private Vector3 Direction;
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = Direction * Speed;
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        Direction  = direction;
+    }
+
+    public void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+}
--- RandomStuff/JhonMovement.cs
+++ RandomStuff/JhonMovement.cs
@@ -16,16 +16,20 @@ public class JhonMovement : MonoBehaviour
 
     //RigidBody 2D es un componente en Unity, que le asignamos a un objeto para que pueda tener propiedades de un objeto 2D, con objeto no me refiero a Objeto de Programación
     private Rigidbody2D Rigidbody2D;
-    private float Horizontal;
 
+    //Animator es el componente en Unity para las animaciones, al igual que RigidBody
+    private Animator Animator;
+    private float Horizontal;
+    public GameObject BulletPrefab;
+    private float LastShoot;
 
     //Cada que inicie el juego, haz:...
     void Start()
     {   
-        //Cada que inice, que me de el componente de RigidBody2D
+        //Cada que inice, que me de el componente de RigidBody2D, Animator
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }   
-
 
     //Cada que se actualice un frame, es decir, esta función se va a ejecutar n veces por segundo. Por ejemplo, si nuestro juego va a 60fps, 60 frames per second, esta f(x) se va a ejecutar 60 veces por segundo.
     void Update()
@@ -33,9 +37,15 @@ public class JhonMovement : MonoBehaviour
         //Horizontal = El input del teclado, obtenemos valores de -1 a 1, dependiendo de la tecla que presione
         Horizontal = Input.GetAxisRaw("Horizontal");
 
+        //Set bool porque en Unity pusimos Bool, el nombre running es el nombre que pusimos, si horizontal es diferente de 0 devuelve true, si es igual, devuelve false
+        Animator.SetBool("Running", Horizontal != 0.0f);
+
         //Dibujamos un rayito que se origina en la hitbox del personaje
         Debug.DrawRay(transform.position, Vector2.down * 0.1f, Color.red); 
 
+        //Ifs pora que cuando el personaje se mueva hacia la izquierda, realmente voltee
+        if(Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f,1.0f,1.0f);
+        else if(Horizontal > 0.0f) transform.localScale = new Vector3(1.0f,1.0f,1.0f);
 
         //Si el rayito de la hitbox choca en el suelo, grounded = true, si no, es false, esto es para que no salte si no está en el suelo, si no se hace esto, el jugador puede
         //dar los saltos que quiera hasta volar XD
@@ -57,6 +67,12 @@ public class JhonMovement : MonoBehaviour
         {
             Jump();
         }
+
+        if(Input.GetKey(KeyCode.Space) && Time.time > LastShoot + 0.25f)
+        {
+            Shoot();
+            LastShoot = Time.time;
+        }
     }
 
     //Esta función es para que la velocidad del personaje no cambie a pesar de cualquier variación de fps, porque si el juego aumenta o disminuye de fps dependiendo del usuario, la velocidad
@@ -74,5 +90,17 @@ public class JhonMovement : MonoBehaviour
     {   
         //Al muñequito, le agregamos una fuerza, que será en el vector2.up, es decir, en el eje de las y; multiplicado por la fuerza que nosotros le demos en Unity.
         Rigidbody2D.AddForce(Vector2.up * jumpforce);
+    }
+
+    private void Shoot()
+    {   
+        Vector3 direction;
+        if(transform.localScale.x == 1) direction = Vector2.right;
+        else direction = Vector2.left;
+
+        //Lo que hace es Instanciar, que toma un prefab y lo duplica en un sitio
+        //del mundo, con alguna rotación, Quaternion.identity es ninguna rotación
+        GameObject bullet = Instantiate(BulletPrefab,transform.position + direction * 0.1f, Quaternion.identity);
+        bullet.GetComponent<BulletScript>().SetDirection(direction);
     }
 }
